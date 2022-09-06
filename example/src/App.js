b6ef8e8206f6405c0cfc290a45e727f114d0ad50
Diego Martín Pérez <diego.martin.perez5@gmail.com>
2022-09-06 19:21:50 +0200

    Initial commit code.

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import ReactCalendar from 'calendar-react'
+import 'calendar-react/dist/index.css'
+
+const App = () => {
+  return <ReactCalendar text="Create React Library Example 😄" />
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const ReactCalendar = ({ text }) => {
+  return (
+    <div>
+      Esto es un componente
+      <br />
+      Example Component: {text}
+    </div>
+  )
+}
+
+export default ReactCalendar
