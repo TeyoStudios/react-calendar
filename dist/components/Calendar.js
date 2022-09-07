3e46ec1d8d523782daac7a5b3683e043370700c5
Diego Martín Pérez <diego.martin.perez5@gmail.com>
2022-09-07 20:01:52 +0200

    [Upd] Update: 	- Created dist folder. 	- Deleted previous configuration files. 	- Added Sass

---
+++ dist/components/Calendar.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+const Calendar = props => {
+  return /*#__PURE__*/React.createElement("div", null, "Esto ser\xEDa el calendario.");
+};
+
+var _default = Calendar;
+exports.default = _default;
